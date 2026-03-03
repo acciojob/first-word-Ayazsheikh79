59be00208ab6f7ecf494b40447da130c14a1b847
Mohammad Ayaz <91006780+Ayazsheikh79@users.noreply.github.com>
2026-03-03 13:41:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function firstWord(s) {
   // your code here
+	s = s.trim()
+	const spaceIndex = s.indexOf(' ')
+	if (spaceIndex === -1) return s
+	return s.slice(0, spaceIndex)
 }
 
 // Do not change the code below
